a7c7b9709c206cb550d96fcb060203b99110e952
Jean-Baptiste Blanc <jbblanc@gmail.com>
2013-11-12 11:14:20 +0000

    refactored problem 1

--- problem1.rb
+++ problem1.rb
@@ -1,14 +1,13 @@
 i=0
 count=0
 while i < 1000
-
-if i%3 ==0
-	puts i.to_s
-	count = count + i
-elsif i%5 ==0
-	puts i.to_s
-	count = count + i
-end
-i = i + 1
+	if i%3 ==0
+		puts i.to_s
+		count = count + i
+	elsif i%5 ==0
+		puts i.to_s
+		count = count + i
+	end
+	i = i + 1
 end
 puts "the sum is #{count}"
