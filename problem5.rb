581e79a8edd3a94d9224cc1237a552be5d4115fa
Jean-Baptiste Blanc <jbblanc@gmail.com>
2013-11-12 14:36:30 +0000

    in a single line

--- problem5.rb
+++ problem5.rb
@@ -5,24 +5,30 @@ primes =[]
 number=1
 n=1
 
-while n <= 20
+while n <= 10
 	primes << Prime.prime_division(n)
-	
 	n = n + 1
 end
 
-primes.each {|prime,division| 
+puts primes.inspect
+
+primes.each  do |prime,division| 
 
 	if not result.include? prime
-		result << prime
+		result << prime		
+		puts prime
 	end
-	}
+end
 
 #print result
-result.each{ |prime,times| 
+result.each do |prime,times| 
 	if not prime.nil?
-	number = number * prime
+		number = number * prime
+	end
 end
-}
 
 puts number
+
+# or the fast way 
+# (1..10).map{|n| Prime.prime_division(n) }.flatten.uniq
+(1..100000).map{|n| Prime.prime_division(n) }.flatten(1).uniq.map(&:first).inject(:*)
